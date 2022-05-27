fc0d48c56d4faf95a3c2a0749da1afdbdf27f9b1
reksa_assidik <reksa_qodri_assidik@teknokrat.ac.id>
2022-05-27 21:42:41 +0700

    menambahkan syntax aritmatika

---
+++ main.py
@@ -0,0 +1,17 @@
+print ("===========rumus aritmatika==========")
+
+x = (input("masukkan nilai x    = "))
+y = (input("masukkan nilai y    ="))
+
+konversi_x = (int(x))
+konversi_y = (int(y))
+nilai = x + y 
+print(nilai)
+
+if konversi_x == 0:
+    print("kalo nilai 0 ya hasilnya 0 juga gembel")
+if konversi_y == 0:
+    print("kalo nilai 0 ya hasilnya 0 juga gembel")
+
+else :
+    print(" ")
